2555a1c4425184df961b5cb68b5e0fd10bb81b5f
julianapvacari <88677370+julianapvacari@users.noreply.github.com>
2022-02-02 11:49:29 -0300

    Add files via upload

---
+++ list.rb
@@ -0,0 +1,23 @@
+#sorting list in ascending order
+
+def sorting_list
+
+puts "Enter your list"
+
+number1 = gets
+number2 = gets
+number3 = gets
+number4 = gets
+number5 = gets
+ 
+new_array = [number1.to_f, number2.to_f, number3.to_f, number4.to_f, number5.to_f] 
+
+puts "List in ascending order"
+
+puts new_array.sort
+
+
+
+end
+
+sorting_list
