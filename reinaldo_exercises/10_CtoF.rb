c07dfff4b347965e127f1d01b9c3495e5548f307
julianapvacari <88677370+julianapvacari@users.noreply.github.com>
2022-02-07 12:11:50 -0300

    Ex9&10_Complete

---
+++ reinaldo_exercises/10_CtoF.rb
@@ -0,0 +1,23 @@
+#Celsius to Fahrenheit
+
+
+def c_to_f
+
+    puts "Enter temperature in Celsius:"
+    
+        c = gets
+    
+    
+       f = c.to_f * 1.8
+    
+       fahrenheit = f.to_f + 32
+    
+    
+    puts "Temperature em Celsius is: #{fahrenheit} ºF"
+    
+    
+    
+    end
+    
+    
+    c_to_f
---
+++ reinaldo_exercises/9_FtoC.rb
@@ -0,0 +1,23 @@
+#Fahrenheit to Celsius
+
+
+def f_to_c
+
+puts "Enter temperature in Fahrenheit:"
+
+    f = gets
+
+
+   c = f.to_f - 32
+
+   celsius = c.to_f/1.8
+
+
+puts "Temperature em Celsius is: #{celsius} ºC"
+
+
+
+end
+
+
+f_to_c
