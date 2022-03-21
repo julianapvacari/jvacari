1a84dc20080d19c27af3095b2a89ad5c809b9123
julianapvacari <88677370+julianapvacari@users.noreply.github.com>
2022-03-21 11:45:03 -0300

    2List_1Ex_Complete

---
+++ reinaldo_exercises/2_List/1_bigger.rb
@@ -0,0 +1,32 @@
+# 1. Make a Program that asks for two numbers and prints the largest of them.
+
+def bigger_then
+
+
+    puts "Enter a number:"
+
+    numb1 = gets.chomp.to_f
+
+    puts "Enter another number:"
+
+    numb2 = gets.chomp.to_f
+
+
+if (numb1 > numb2)
+
+    puts "The bigger number is #{numb1}"
+
+
+
+elsif (numb1 < numb2)
+
+    puts "The bigger number is #{numb2}"
+    
+
+end
+    
+
+
+end
+
+bigger_then
