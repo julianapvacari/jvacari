27d68d89fec00bbdf8dd780d25aaf22d14d8a319
julianapvacari <88677370+julianapvacari@users.noreply.github.com>
2022-03-27 13:37:15 -0300

    2List_3Ex_Complete

---
+++ Ruby/2_List/5_notes.rb
@@ -0,0 +1,16 @@
+#5. Make a program for reading a student's two partial grades. The program must calculate the
+#average achieved per student and present:
+#- The message "Approved", if the average reached is greater than or equal to seven;
+#- The message "Fail", if the average is less than seven;
+#- The message "Approved with Distinction", if the average is equal to ten.
+
+def note
+
+
+
+
+
+
+end
+
+note
