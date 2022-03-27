e77100697ec21165236cadb2cf7a5e8408e22ab4
julianapvacari <88677370+julianapvacari@users.noreply.github.com>
2022-03-27 12:38:04 -0300

    2List_2and3_Complete

---
+++ Ruby/2_List/3_f_or_m.rb
@@ -0,0 +1,41 @@
+#3. Make a Program that checks if a typed letter is "F" or "M". As the letter write: F -
+#Female, M - Male, Gender Invalid.
+
+def f_or_m 
+
+ puts "Enters the gender with f for Female and m for Male:"
+ 
+ STDOUT.flush
+
+  gender = gets.chomp
+
+
+  if (gender == "f")
+     puts "Female (Gender is valid)" 
+
+  elsif (gender == "m")
+     puts "Male (Gender is valid)"
+
+  else (gender)
+     puts "Invalid gender."
+
+end
+
+
+#   if (gender == "F")
+#    puts "Female (Gender is valid)"
+
+#   elsif (gender == "M")
+#    puts "Male (Gender is valid)"
+
+# #   else (gender)
+# #    puts "Invalid gender."
+
+# end
+
+
+
+
+end
+
+f_or_m
