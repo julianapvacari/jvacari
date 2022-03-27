f463d80bafdfa4f6c6686bc23cf626585b0491d5
julianapvacari <88677370+julianapvacari@users.noreply.github.com>
2022-03-27 12:38:35 -0300

    2List_2and3_Complete

---
+++ Ruby/2_List/2_positive_or_negative.rb
@@ -0,0 +1,31 @@
+#2. Write a Program that asks for a value and shows on the screen if the value is positive or negative.
+
+def positive_or_negative 
+
+
+    puts "Enters the number and I will shows up if they are positive or negative!"
+
+
+    number = gets.chomp.to_f
+
+
+
+    if (number >= 0)
+        puts "The number is positive!"
+
+    elsif (number <= -1)
+        puts "The number is negative!"
+
+
+
+
+
+    end
+
+end
+
+positive_or_negative
+
+
+
+
